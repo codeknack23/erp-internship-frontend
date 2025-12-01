3ff52647be72fe78a58ad6b3dacf76feadcde521
ronak <ronakbhojayya@gmail.com>
2025-12-01 14:03:44 +0530

    first commit

--- src/pages/VendorList.jsx
+++ src/pages/VendorList.jsx
@@ -11,8 +11,11 @@ export default function VendorList() {
   const [loadingId, setLoadingId] = useState(null); // Button loader
   const navigate = useNavigate();
 
-  // Simple spinner
+
   const Spinner = () => (
+    <span className="inline-block w-4 h-4 border-2 border-black border-t-transparent rounded-full animate-spin"></span>
+  );
+  const Spinner2 = () => (
     <span className="inline-block w-4 h-4 border-2 border-black border-t-transparent rounded-full animate-spin"></span>
   );
 
@@ -128,7 +131,7 @@ export default function VendorList() {
                           >
                             {loadingId === v._id ? (
                               <>
-                                <Spinner />
+                                <Spinner2 />
                               </>
                             ) : (
                               "Deactivate"
@@ -142,7 +145,7 @@ export default function VendorList() {
                           >
                             {loadingId === v._id ? (
                               <>
-                                <Spinner />
+                                <Spinner2 />
                               </>
                             ) : (
                               "Activate"
